638ac51c56f7258ec18799bfcfd5261ee1e997ef
Tom Kwong <tk3369@gmail.com>
2020-12-13 23:35:52 -0800

    sync

--- day14.jl
+++ day14.jl
@@ -106,7 +106,7 @@ make_setter(floater, tup) =[floater[i] => tup[i] for i in 1:length(floater)]
 function part2()
     program = read_data2()
     local setter, floater
-    mem = Dict()
+    mem = Dict{Int,Int}()
     for instruction in program
         if instruction.command == :setmask
             setter = instruction.setter
@@ -119,5 +119,68 @@ function part2()
             end
         end
     end
-    return sum(values(mem))
+    return sum(values(mem)), mem
+end
+
+# Visualization
+using Plots
+
+function part2_animated(board_size = 261550)
+    anim = Animation()
+    marker_size = 2.0
+    marker_alpha = 0.8
+    marker_color = :green
+    marker_shape = :rect
+    scatter([1],[1], xlims = (1, board_size), ylims = (1, board_size),
+        markercolor = marker_color,
+        markersize = marker_size,
+        markeralpha = marker_alpha,
+        markershape = marker_shape,
+        markerstrokewidth = 0,
+        legend = nothing, ticks = nothing, showaxis = false, grid = nothing,
+        background_color = :black,
+        foreground_color_axis = :black, gridlinewidth = 0)
+    program = read_data2()
+    local setter, floater
+    mem = Dict{Int,Int}()
+    cnt = 0
+    xs = Int[]
+    ys = Int[]
+    for instruction in program
+        if instruction.command == :setmask
+            setter = instruction.setter
+            floater = instruction.floater
+        else
+            temp_address = apply_bitmask(instruction.address, setter)
+            for bit_tuple in Iterators.product([[0,1] for _ in 1:length(floater)]...)
+                addr = apply_bitmask(temp_address, make_setter(floater, bit_tuple))
+                mem[addr] = instruction.value
+                x, y = addr ÷ board_size, addr % board_size
+                push!(xs, x)
+                push!(ys, y)
+            end
+        end
+        cnt += 1
+        if cnt % 5 == 0
+            scatter!(xs, ys, 
+                markercolor = marker_color,
+                markersize = marker_size,
+                markeralpha = marker_alpha,
+                markershape = marker_shape,
+                markerstrokewidth = 0,
+                title = "AoC Day 14 - Iteration $cnt")
+            frame(anim)
+            resize!(xs, 0)
+            resize!(ys, 0)
+        end
+    end
+    scatter!(xs, ys, 
+        markercolor = marker_color,
+        markersize = marker_size,
+        markeralpha = marker_alpha,
+        markershape = marker_shape,
+        markerstrokewidth = 0,
+        title = "AoC Day 14 - Iteration $cnt")
+    frame(anim)
+    gif(anim, "day14_anim.gif", fps = 15)
 end
